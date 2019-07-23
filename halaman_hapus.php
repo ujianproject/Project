ff86f56094299c4da3099cd54996a51998cc9285
raihanmanis <raihan18ti@mahasiswa.pcr.ac.id>
2019-07-23 19:53:03 +0700

    Create halaman_hapus.php

---
+++ halaman_hapus.php
@@ -0,0 +1,13 @@
+<?php
+	//panggil koneksi
+include("koneksi.php");
+$id = $_GET['no'];
+
+if (!empty($id)) {
+	mysqli_query($db, "DELETE  FROM klinik WHERE no='$id'");
+	header('location: halaman_data1.php?status=sukses');
+	# code...
+}else{
+	header('location: halaman_data1.php?status=gagal');
+}
+?>
