6254d4950713a70bad40c40e0f0339a00f4e2e41
nabilafirdha <nabila.nfa@gmail.com>
2019-07-23 19:51:03 +0700

    Update klinik.php

--- klinik.php
+++ klinik.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- Theme Made By www.w3schools.com -->
-  <title>Pendaftaran Klinik Mekar Sari</title>
+ 
+  <title>Pendaftaran Klinik Mekar sari</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
